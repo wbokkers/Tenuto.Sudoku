69d82862c7a3b17ddc9d0ad3a933f05474989fef
Wim Bokkers <wim.bokkers@xelion.nl>
2020-10-05 22:57:27 +0200

    Some improvements

--- Tenuto.Sudoku.Uwp/Asteroids/GameConstants.cs
+++ Tenuto.Sudoku.Uwp/Asteroids/GameConstants.cs
@@ -4,8 +4,8 @@
     {
         public const float DesignWidth = 800;
         public const float DesignHeight = 600;
-        public const float ShipGhostTime = 2;
+        public const float ShipGhostTime = 1.5f;
         public const int ExtraLives = 2;
-        public const float ExplosionDuration = 1.0f;
+        public const float ExplosionDuration = 0.7f;
     }
 }
